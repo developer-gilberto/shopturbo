7530942af991c6ed809b3b9829362ccb4ce00d2d
developer-gilberto <developer.gilberto@gmail.com>
2025-02-16 22:37:59 -0300

    ...

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,8 @@
-"use client";
-
 import localFont from "next/font/local";
 import "./globals.css";
-import { LoadingProvider } from "@/hooks/LoadingContext";
 import { Container } from "@/components/layout/container";
 import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
-import { useState } from "react";
-import { Loading } from "@/components/ui/loading";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -20,33 +15,20 @@ const geistMono = localFont({
     weight: "100 900",
 });
 
-// export const metadata = {
-//     title: "ShopTurbo",
-//     description: "Seu gerenciador de vendas.",
-// };
+export const metadata = {
+    title: "ShopTurbo",
+    description: "Descomplique suas vendas.",
+};
 
 export default function RootLayout({ children }) {
-    const [isLoading, setIsLoading] = useState(false);
-    // if (isLoading) return <Loading />
-
-    return !isLoading ? (
-        <LoadingProvider>
-            <html lang="pt-br">
-                <body
-                    className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-                >
-                    <Header />
-                    <Container>{children}</Container>
-                    <Footer />
-                </body>
-            </html>
-        </LoadingProvider>
-    ) : (
-        <html lang="pt-br" className="flex justify-center items-center ">
+    return (
+        <html lang="pt-br">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-fit flex justify-center items-center `}
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                <Loading />
+                <Header />
+                <Container>{children}</Container>
+                <Footer />
             </body>
         </html>
     );

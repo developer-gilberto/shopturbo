1b3358b28477301452386ca289422dc2bced62fc
Gilberto Lopes <developer.gilberto@gmail.com>
2025-09-30 21:45:21 -0300

    refactor: replace useSearchParams() with URLSearchParams(window.location.search)

--- src/app/callback/page.jsx
+++ src/app/callback/page.jsx
@@ -1,33 +1,29 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { FeedbackModal } from "@/components/ui/feedback-modal";
 import { Loading } from "@/components/ui/loading";
 
 export default function Callback() {
     const router = useRouter();
-    const searchParams = useSearchParams();
-    const code = searchParams.get("code");
-    const shop_id = searchParams.get("shop_id");
 
     const [feedbackMessage, setFeedbackMessage] = useState("");
     const [successfulRequest, setSuccessfulRequest] = useState(false);
     const [loading, setLoading] = useState(true);
 
-    if (!code || !shop_id) {
-        return (
-            <>
-                <FeedbackModal
-                    request={successfulRequest}
-                    message="'code' e 'shop_id' são obrigratórios."
-                />
-            </>
-        );
-    }
-
     useEffect(() => {
         try {
+            const searchParams = new URLSearchParams(window.location.search);
+            const code = searchParams.get("code");
+            const shop_id = searchParams.get("shop_id");
+
+            if (!code || !shop_id) {
+                return setFeedbackMessage(
+                    "'code' e 'shop_id' são obrigratórios.",
+                );
+            }
+
             const fetchToken = async () => {
                 const response = await fetch(
                     `${process.env.NEXT_PUBLIC_SERVER_URL}/api/shopee/access-token?code=${code}&shop_id=${shop_id}`,
@@ -69,7 +65,7 @@ export default function Callback() {
         } finally {
             setLoading(false);
         }
-    }, [code, shop_id]);
+    }, []);
 
     return (
         <div className="flex flex-col items-center justify-center">

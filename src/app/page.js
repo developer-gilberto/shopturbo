870b4ce29f479adaa6f17542d951cfdc14de326f
Gilberto Lopes <developer.gilberto@gmail.com>
2025-09-30 23:09:37 -0300

    feat: redirects the API response code to the callback route

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,24 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/btn";
 import { Logo } from "@/components/ui/logo";
+import { useEffect } from "react";
 
 export default function Home() {
+    const router = useRouter();
+
+    useEffect(() => {
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = searchParams.get("code");
+        const shop_id = searchParams.get("shop_id");
+
+        if (code && shop_id) {
+            return router.replace(`/callback?code=${code}&shop_id=${shop_id}`);
+        }
+    }, []);
+
     return (
         <div className="h-dvh flex justify-center items-center">
             <div className="p-8 rounded-lg flex flex-col justify-center items-center max-w-fit mx-auto">
